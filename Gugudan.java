42d96ac51e6ada79895a29cdcd32c476755fac20
Ujoonnee <69345466+Ujoonnee@users.noreply.github.com>
2021-11-24 23:12:50 +0900

    Update Gugudan.java
    refactored by using method

--- Gugudan.java
+++ Gugudan.java
@@ -1,24 +1,17 @@
 public class Gugudan {
-  public static void main(String[] args){
-  
-    int x = 2;
-    int y = 1;
 
-    while(x<10){
-      System.out.println(x+"단");
-      while(y < 11){
-        if (y < 10) {
-        System.out.println(x+" * "+y+" = "+x*y);
-        y++;
-        } else {
-            if(y == 10) {
-            y = 1;
-            System.out.println("");
-            break;
-            }
-        }
-        }
-      x++;
-    }
+	public static int[] calc(int num) {
+		int[] result = new int[9];
+		for (int i=0; i<result.length; i++) {
+			result[i] = (i+1)*num;
+		}
+		return result;
+	}
+  public static void main(String[] args){
+
+	  for (int i=1; i<10; i++) {
+		  calc(i);
+		  System.out.println(result);
+	  	}
   }
 }
