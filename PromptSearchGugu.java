db1188775844c742d3d3f0806a0f607b9ef3769b
Ujoonnee <69345466+Ujoonnee@users.noreply.github.com>
2021-11-29 00:08:47 +0900

    Create PromptSearchGugu.java

---
+++ PromptSearchGugu.java
@@ -0,0 +1,27 @@
+import java.util.Scanner;
+
+public class PromptSearchGugu {
+	
+	public String name() {
+		return "SearchGugu";
+	}
+
+	public void main(Scanner sc) {
+
+		while(true) {
+			
+			System.out.println("Table number?");
+			int x = sc.nextInt();
+			int y = 1;
+			if (x != 0) {
+				System.out.println("Table number" + x);
+				while (y < 10) {
+					System.out.println(x + " * " + y + " = " + x * y);
+					y++;
+				}
+			} else {
+				break;
+			}
+		}
+	}
+}
