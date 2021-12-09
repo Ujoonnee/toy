3e208b0793a297ac40a74be13dd5c341fc08b8a5
Ujoonnee <69345466+Ujoonnee@users.noreply.github.com>
2021-12-09 18:04:35 +0900

    Update PromptPlanner

--- Prompt.java
+++ Prompt.java
@@ -1,18 +1,20 @@
+import java.io.IOException;
 import java.util.Scanner;
 
 public class Prompt {
 	
-	static PromptCalendar cal = new PromptCalendar();
+	static PromptCalendar calendar = new PromptCalendar();
 	static PromptSearchGugu gugu = new PromptSearchGugu();
+	static PromptPlanner planner = new PromptPlanner();
 	
 	// to add more classes
-	static String menu1 = "1. " + cal.name()+ " ";
+	static String menu1 = "1. " + calendar.name()+ " ";
 	static String menu2 = "2. " + gugu.name()+ " ";
-	static String menu3 = "";
+	static String menu3 = "3. " + planner.name()+ " ";
 	static String menu4 = "";
 	static String menu5 = "";
 	
-	public static int menu() {
+	public static int menu() throws IOException {
 
 		Scanner sc = new Scanner(System.in);
 		
@@ -20,13 +22,14 @@ public class Prompt {
 		int output = 1;
 		
 		if (input == 1) {
-			cal.main(sc);
+			calendar.main(sc);
 			System.out.println("\nCalendar ended\n");
 		} else if (input == 2) {
 			gugu.main(sc);
 			System.out.println("\nSearchGugu ended\n");
 		} else if (input == 3) {
-			
+			planner.main(sc);
+			System.out.println("\nPlanner ended\n");
 		} else if (input == 4) {
 			
 		} else if (input == 5) {
@@ -37,7 +40,7 @@ public class Prompt {
 		return output;
 	}
 	
-	public static void main(String[] args) {
+	public static void main(String[] args) throws IOException {
 		
 		while(true) {
 			System.out.println(menu1 + menu2 + menu3 + menu4 + menu5 + "0. exit");
